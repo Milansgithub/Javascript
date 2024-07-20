4902952935a2086f256feeeda9f7d549d3b9bc07
Milan <160263928+Milansgithub@users.noreply.github.com>
2024-07-20 17:55:59 +0530

    Create 04_primitives.js

---
+++ practice Sets/04_primitives.js
@@ -0,0 +1,21 @@
+// null 
+// Number
+// str
+// symbols
+// undefined
+// Boolean
+// BigInt
+
+// nn bb ss u
+
+let a = null;
+let b = "Milan";
+let c = true;
+let d = BigInt("3444")
+let e = 3434
+let f = Symbol("i am a symbol")
+let g = undefined
+console.log(a,b,c,d,e,f)
+
+console.log(typeof e)
+// we can use typeof to check the type of variable
